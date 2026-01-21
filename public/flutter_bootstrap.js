83ddf692e1a11017b1af20c67413a5448d6aaba8
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-01-21 16:36:01 +0530

    Update Flutter web build for Vercel

--- public/flutter_bootstrap.js
+++ public/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3754738475"
+    serviceWorkerVersion: "3908630796"
   }
 });
