4376d391c4b94d3a57fa46de8a48a4b485eb3a10
Abdulsathar27 <abdulsatharnechithodi@gmail.com>
2026-01-21 16:37:13 +0530

    Auto deploy Flutter web

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -28,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2757ac48e59e0a578cc245bd16a7aba3
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "385e10694d88f0fe7586d5383f808a90",
+"flutter_bootstrap.js": "2938f241d0f063952123ee9860d11075",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
